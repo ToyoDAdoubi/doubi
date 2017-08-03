79757d43854ad52c90693e9b0387d72617458e7a
Toyo <toyo13140010096@gmail.com>
2017-08-03 12:32:17 +0800

    # v1.0.6 新增 运行脚本 install 参数可以覆盖安装或者说更新

--- caddy_install.sh
+++ caddy_install.sh
@@ -4,13 +4,12 @@ export PATH
 #=================================================
 #       System Required: CentOS/Debian/Ubuntu
 #       Description: Caddy Install
-#       Version: 1.0.5
+#       Version: 1.0.6
 #       Author: Toyo
 #       Blog: https://doub.io/shell-jc1/
 #=================================================
 file="/usr/local/caddy/"
 caddy_file="/usr/local/caddy/caddy"
-caddy_ver_file="/usr/local/caddy/ver.txt"
 caddy_conf_file="/usr/local/caddy/Caddyfile"
 Info_font_prefix="\033[32m" && Error_font_prefix="\033[31m" && Info_background_prefix="\033[42;37m" && Error_background_prefix="\033[41;37m" && Font_suffix="\033[0m"
 
@@ -38,6 +37,8 @@ check_installed_status(){
 Download_caddy(){
 	[[ ! -e ${file} ]] && mkdir "${file}"
 	cd "${file}"
+	PID=$(ps -ef |grep "caddy" |grep -v "grep" |grep -v "init.d" |grep -v "service" |grep -v "caddy_install" |awk '{print $2}')
+	[[ ! -z ${PID} ]] && kill -9 ${PID}
 	[[ -e "caddy_linux*.tar.gz" ]] && rm -rf "caddy_linux*.tar.gz"
 	[[ ! -z ${extension} ]] && extension_all="?plugins=${extension}"
 	if [[ ${bit} == "i386" ]]; then
@@ -76,16 +77,20 @@ Service_caddy(){
 	fi
 }
 install_caddy(){
-	[[ -e ${caddy_file} ]] && echo -e "${Error_font_prefix}[错误]${Font_suffix} 检测到 Caddy 已安装，如需继续，请先卸载 !" && exit 1
-	check_sys
+	if [[ -e ${caddy_file} ]]; then
+		echo && echo -e "${Error_font_prefix}[信息]${Font_suffix} 检测到 Caddy 已安装，是否继续安装(覆盖更新)？[y/N]"
+		stty erase '^H' && read -p "(默认: n):" yn
+		[[ -z ${yn} ]] && yn="n"
+		if [[ ${yn} == [Nn] ]]; then
+			echo && echo "已取消..." && exit 1
+		fi
+	fi
 	Download_caddy
 	Service_caddy
-	echo && echo -e " Caddy 配置文件：${caddy_conf_file}
- ${Info_font_prefix}[信息]${Font_suffix} Caddy 安装完成！" && echo
+	echo && echo -e " Caddy 配置文件：${caddy_conf_file} \n 使用说明：service caddy start | stop | restart | status \n ${Info_font_prefix}[信息]${Font_suffix} Caddy 安装完成！" && echo
 }
 uninstall_caddy(){
 	check_installed_status
-	check_sys
 	echo && echo "确定要卸载 Caddy ? [y/N]"
 	stty erase '^H' && read -p "(默认: n):" unyn
 	[[ -z ${unyn} ]] && unyn="n"
@@ -106,6 +111,7 @@ uninstall_caddy(){
 		echo && echo "卸载已取消..." && echo
 	fi
 }
+check_sys
 action=$1
 extension=$2
 [[ -z $1 ]] && action=install
