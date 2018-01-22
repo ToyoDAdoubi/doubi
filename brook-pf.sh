0552c7de4ce1adfcf8ec26649e85716812d7d830
Toyo <toyo13140010096@gmail.com>
2018-01-22 22:50:59 +0800

    # v1.0.1

--- brook-pf.sh
+++ brook-pf.sh
@@ -5,12 +5,12 @@ export PATH
 #=================================================
 #	System Required: CentOS/Debian/Ubuntu
 #	Description: Brook
-#	Version: 1.0.0
+#	Version: 1.0.1
 #	Author: Toyo
 #	Blog: https://doub.io/wlzy-jc37/
 #=================================================
 
-sh_ver="1.0.0"
+sh_ver="1.0.1"
 file="/usr/local/brook-pf"
 brook_file="/usr/local/brook-pf/brook"
 brook_conf="/usr/local/brook-pf/brook.conf"
@@ -48,13 +48,24 @@ check_pid(){
 	PID=`ps -ef| grep "brook relays"| grep -v grep| grep -v ".sh"| grep -v "init.d"| grep -v "service"| awk '{print $2}'`
 }
 check_new_ver(){
-	brook_new_ver=`wget -qO- https://github.com/txthinking/brook/tags| grep "/txthinking/brook/releases/tag/"| head -n 1| awk -F "/tag/" '{print $2}'| sed 's/\">//'`
-	if [[ -z ${brook_new_ver} ]]; then
-		echo -e "${Error} Brook 最新版本获取失败，请手动获取最新版本号[ https://github.com/txthinking/brook/releases ]"
-		stty erase '^H' && read -p "请输入版本号 [ 格式是日期 , 如 v20170330 ] :" brook_new_ver
-		[[ -z "${brook_new_ver}" ]] && echo "取消..." && exit 1
+	if [[ "${Download_type}" == "1" ]]; then
+		brook_new_ver=$(wget -qO- "https://softs.fun/?dir=%E7%A7%91%E5%AD%A6%E4%B8%8A%E7%BD%91/PC/Brook/Linux"|grep 'data-name="Brook-x64-v'|head -n 1|awk -F 'Linux/Brook-x64-' '{print $2}'|sed 's/\">//')
+		if [[ -z ${brook_new_ver} ]]; then
+			echo -e "${Error} Brook 最新版本获取失败，请手动获取最新版本号[ https://github.com/txthinking/brook/releases ]"
+			stty erase '^H' && read -p "请输入版本号 [ 格式是日期 , 如 v20170330 ] :" brook_new_ver
+			[[ -z "${brook_new_ver}" ]] && echo "取消..." && exit 1
+		else
+			echo -e "${Info} 检测到 Brook 最新版本为 [ ${brook_new_ver} ]"
+		fi
 	else
-		echo -e "${Info} 检测到 Brook 最新版本为 [ ${brook_new_ver} ]"
+		brook_new_ver=$(wget -qO- "https://github.com/txthinking/brook/tags"| grep "/txthinking/brook/releases/tag/"| head -n 1| awk -F "/tag/" '{print $2}'| sed 's/\">//')
+		if [[ -z ${brook_new_ver} ]]; then
+			echo -e "${Error} Brook 最新版本获取失败，请手动获取最新版本号[ https://github.com/txthinking/brook/releases ]"
+			stty erase '^H' && read -p "请输入版本号 [ 格式是日期 , 如 v20170330 ] :" brook_new_ver
+			[[ -z "${brook_new_ver}" ]] && echo "取消..." && exit 1
+		else
+			echo -e "${Info} 检测到 Brook 最新版本为 [ ${brook_new_ver} ]"
+		fi
 	fi
 }
 check_ver_comparison(){
@@ -78,29 +89,56 @@ check_ver_comparison(){
 }
 Download_brook(){
 	cd ${file}
-	if [ ${bit} == "x86_64" ]; then
-		wget --no-check-certificate -N "https://github.com/txthinking/brook/releases/download/${brook_new_ver}/brook"
+	if [[ "${Download_type}" == "1" ]]; then
+		if [[ ${bit} == "x86_64" ]]; then
+			wget --no-check-certificate -N "https://softs.fun/%E7%A7%91%E5%AD%A6%E4%B8%8A%E7%BD%91/PC/Brook/Linux/Brook-x64-${brook_new_ver}"
+			mv "Brook-x64-${brook_new_ver}" brook
+		else
+			wget --no-check-certificate -N "https://softs.fun/%E7%A7%91%E5%AD%A6%E4%B8%8A%E7%BD%91/PC/Brook/Linux/Brook-x32-${brook_new_ver}"
+			mv "Brook-x32-${brook_new_ver}" brook
+		fi
 	else
-		wget --no-check-certificate -N "https://github.com/txthinking/brook/releases/download/${brook_new_ver}/brook_linux_386"
-		mv brook_linux_386 brook
+		if [[ ${bit} == "x86_64" ]]; then
+			wget --no-check-certificate -N "https://github.com/txthinking/brook/releases/download/${brook_new_ver}/brook"
+		else
+			wget --no-check-certificate -N "https://github.com/txthinking/brook/releases/download/${brook_new_ver}/brook_linux_386"
+			mv brook_linux_386 brook
+		fi
 	fi
 	[[ ! -e "brook" ]] && echo -e "${Error} Brook 下载失败 !" && exit 1
 	chmod +x brook
 }
 Service_brook(){
-	if [[ ${release} = "centos" ]]; then
-		if ! wget --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/other/brook-pf_centos -O /etc/init.d/brook-pf; then
-			echo -e "${Error} Brook服务 管理脚本下载失败 !" && exit 1
+	if [[ "${Download_type}" == "1" ]]; then
+		if [[ ${release} = "centos" ]]; then
+			if ! wget --no-check-certificate "https://softs.fun/Bash/other/brook-pf_centos" -O /etc/init.d/brook-pf; then
+				echo -e "${Error} Brook服务 管理脚本下载失败 !" && exit 1
+			fi
+			chmod +x /etc/init.d/brook-pf
+			chkconfig --add brook-pf
+			chkconfig brook-pf on
+		else
+			if ! wget --no-check-certificate "https://softs.fun/Bash/other/brook-pf_debian" -O /etc/init.d/brook-pf; then
+				echo -e "${Error} Brook服务 管理脚本下载失败 !" && exit 1
+			fi
+			chmod +x /etc/init.d/brook-pf
+			update-rc.d -f brook-pf defaults
 		fi
-		chmod +x /etc/init.d/brook-pf
-		chkconfig --add brook-pf
-		chkconfig brook-pf on
 	else
-		if ! wget --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/other/brook-pf_debian -O /etc/init.d/brook-pf; then
-			echo -e "${Error} Brook服务 管理脚本下载失败 !" && exit 1
+		if [[ ${release} = "centos" ]]; then
+			if ! wget --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/other/brook-pf_centos -O /etc/init.d/brook-pf; then
+				echo -e "${Error} Brook服务 管理脚本下载失败 !" && exit 1
+			fi
+			chmod +x /etc/init.d/brook-pf
+			chkconfig --add brook-pf
+			chkconfig brook-pf on
+		else
+			if ! wget --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/other/brook-pf_debian -O /etc/init.d/brook-pf; then
+				echo -e "${Error} Brook服务 管理脚本下载失败 !" && exit 1
+			fi
+			chmod +x /etc/init.d/brook-pf
+			update-rc.d -f brook-pf defaults
 		fi
-		chmod +x /etc/init.d/brook-pf
-		update-rc.d -f brook-pf defaults
 	fi
 	echo -e "${Info} Brook服务 管理脚本下载完成 !"
 }
@@ -418,10 +456,24 @@ Install_brook(){
 	[[ -e ${brook_file} ]] && echo -e "${Error} 检测到 Brook 已安装 !" && exit 1
 	echo -e "${Info} 开始安装/配置 依赖..."
 	Installation_dependency
+	
+	echo && echo -e "请选择你的服务器是国内还是国外
+ ${Green_font_prefix}1.${Font_color_suffix}  国内服务器(逗比云)
+ ${Green_font_prefix}2.${Font_color_suffix}  国外服务器(Github)
+ 
+ ${Tip} 因为国内对 Github 限速，这会导致国内服务器下载速度极慢，所以选择 国内服务器 选项就会从我的 逗比云 下载!" && echo
+	stty erase '^H' && read -p "(默认: 2 国外服务器):" bk_Download
+	[[ -z "${bk_Download}" ]] && bk_Download="2"
+	if [[ ${bk_Download} == "1" ]]; then
+		Download_type="1"
+	else
+		Download_type="2"
+	fi
 	echo -e "${Info} 开始检测最新版本..."
 	check_new_ver
 	echo -e "${Info} 开始下载/安装..."
 	Download_brook
+	
 	echo -e "${Info} 开始下载/安装 服务脚本(init)..."
 	Service_brook
 	echo -e "${Info} 开始写入 配置文件..."
@@ -450,6 +502,18 @@ Restart_brook(){
 }
 Update_brook(){
 	check_installed_status
+	echo && echo -e "请选择你的服务器是国内还是国外
+ ${Green_font_prefix}1.${Font_color_suffix}  国内服务器(逗比云)
+ ${Green_font_prefix}2.${Font_color_suffix}  国外服务器(Github)
+ 
+ ${Tip} 因为国内对 Github 限速，这会导致国内服务器下载速度极慢，所以选择 国内服务器 选项就会从我的 逗比云 下载!" && echo
+	stty erase '^H' && read -p "(默认: 2 国外服务器):" bk_Download
+	[[ -z "${bk_Download}" ]] && bk_Download="2"
+	if [[ ${bk_Download} == "1" ]]; then
+		Download_type="1"
+	else
+		Download_type="2"
+	fi
 	check_new_ver
 	check_ver_comparison
 }
