ead2488a094f6e9b371f401c9e9e22e2bf8c8392
Toyo <toyo13140010096@gmail.com>
2018-01-22 19:35:36 +0800

    # v1.1.3

--- aria2.sh
+++ aria2.sh
@@ -5,11 +5,11 @@ export PATH
 #=================================================
 #	System Required: CentOS/Debian/Ubuntu
 #	Description: Aria2
-#	Version: 1.1.2
+#	Version: 1.1.3
 #	Author: Toyo
 #	Blog: https://doub.io/shell-jc4/
 #=================================================
-sh_ver="1.1.2"
+sh_ver="1.1.3"
 file="/root/.aria2"
 aria2_conf="/root/.aria2/aria2.conf"
 aria2_log="/root/.aria2/aria2.log"
@@ -126,6 +126,7 @@ Install_aria2(){
 	echo -e "${Info} 开始下载/安装 服务脚本(init)..."
 	Service_aria2
 	Read_config
+	aria2_RPC_port=${aria2_port}
 	echo -e "${Info} 开始设置 iptables防火墙..."
 	Set_iptables
 	echo -e "${Info} 开始添加 iptables防火墙规则..."
@@ -155,15 +156,217 @@ Restart_aria2(){
 }
 Set_aria2(){
 	check_installed_status
+	echo && echo -e "你要做什么？
+ ${Green_font_prefix}1.${Font_color_suffix}  修改 Aria2 RPC密码
+ ${Green_font_prefix}2.${Font_color_suffix}  修改 Aria2 RPC端口
+ ${Green_font_prefix}3.${Font_color_suffix}  修改 Aria2 文件下载位置
+ ${Green_font_prefix}4.${Font_color_suffix}  修改 Aria2 密码+端口+文件下载位置
+ ${Green_font_prefix}5.${Font_color_suffix}  手动 打开配置文件修改" && echo
+	stty erase '^H' && read -p "(默认: 取消):" aria2_modify
+	[[ -z "${aria2_modify}" ]] && echo "已取消..." && exit 1
+	if [[ ${aria2_modify} == "1" ]]; then
+		Set_aria2_RPC_passwd
+	elif [[ ${aria2_modify} == "2" ]]; then
+		Set_aria2_RPC_port
+	elif [[ ${aria2_modify} == "3" ]]; then
+		Set_aria2_RPC_dir
+	elif [[ ${aria2_modify} == "4" ]]; then
+		Set_aria2_RPC_passwd_port_dir
+	elif [[ ${aria2_modify} == "5" ]]; then
+		Set_aria2_vim_conf
+	else
+		echo -e "${Error} 请输入正确的数字(1-4)" && exit 1
+	fi
+}
+Set_aria2_RPC_passwd(){
+	read_123=$1
+	if [[ ${read_123} != "1" ]]; then
+		Read_config
+	fi
+	if [[ -z "${aria2_passwd}" ]]; then
+		aria2_passwd_1="空(没有检测到配置，可能手动删除或注释了)"
+	else
+		aria2_passwd_1=${aria2_passwd}
+	fi
+	echo -e "请输入要设置的 Aria2 RPC密码(旧密码为：${Green_font_prefix}${aria2_passwd_1}${Font_color_suffix})"
+	stty erase '^H' && read -p "(默认密码: doub.io 密码请不要包含等号 = 和井号 #):" aria2_RPC_passwd
+	echo
+	[[ -z "${aria2_RPC_passwd}" ]] && aria2_RPC_passwd="doub.io"
+	if [[ "${aria2_passwd}" != "${aria2_RPC_passwd}" ]]; then
+		if [[ -z "${aria2_passwd}" ]]; then
+			echo -e "\nrpc-secret=${aria2_RPC_passwd}" >> ${aria2_conf}
+			if [[ $? -eq 0 ]];then
+				echo -e "${Info} 密码修改成功！新密码为：${Green_font_prefix}${aria2_RPC_passwd}${Font_color_suffix}(因为找不到旧配置参数，所以自动加入配置文件底部)"
+				if [[ ${read_123} != "1" ]]; then
+					Restart_aria2
+				fi
+			else 
+				echo -e "${Error} 密码修改失败！旧密码为：${Green_font_prefix}${aria2_passwd}${Font_color_suffix}"
+			fi
+		else
+			sed -i 's/^rpc-secret='${aria2_passwd}'/rpc-secret='${aria2_RPC_passwd}'/g' ${aria2_conf}
+			if [[ $? -eq 0 ]];then
+				echo -e "${Info} 密码修改成功！新密码为：${Green_font_prefix}${aria2_RPC_passwd}${Font_color_suffix}"
+				if [[ ${read_123} != "1" ]]; then
+					Restart_aria2
+				fi
+			else 
+				echo -e "${Error} 密码修改失败！旧密码为：${Green_font_prefix}${aria2_passwd}${Font_color_suffix}"
+			fi
+		fi
+	else
+		echo -e "${Error} 新密码与旧密码一致，取消..."
+	fi
+}
+Set_aria2_RPC_port(){
+	read_123=$1
+	if [[ ${read_123} != "1" ]]; then
+		Read_config
+	fi
+	if [[ -z "${aria2_port}" ]]; then
+		aria2_port_1="空(没有检测到配置，可能手动删除或注释了)"
+	else
+		aria2_port_1=${aria2_port}
+	fi
+	echo -e "请输入要设置的 Aria2 RPC端口(旧端口为：${Green_font_prefix}${aria2_port_1}${Font_color_suffix})"
+	stty erase '^H' && read -p "(默认端口: 6800):" aria2_RPC_port
+	echo
+	[[ -z "${aria2_RPC_port}" ]] && aria2_RPC_port="6800"
+	if [[ "${aria2_port}" != "${aria2_RPC_port}" ]]; then
+		if [[ -z "${aria2_port}" ]]; then
+			echo -e "\nrpc-listen-port=${aria2_RPC_port}" >> ${aria2_conf}
+			if [[ $? -eq 0 ]];then
+				echo -e "${Info} 端口修改成功！新端口为：${Green_font_prefix}${aria2_RPC_port}${Font_color_suffix}(因为找不到旧配置参数，所以自动加入配置文件底部)"
+				Del_iptables
+				Add_iptables
+				Save_iptables
+				if [[ ${read_123} != "1" ]]; then
+					Restart_aria2
+				fi
+			else 
+				echo -e "${Error} 端口修改失败！旧端口为：${Green_font_prefix}${aria2_port}${Font_color_suffix}"
+			fi
+		else
+			sed -i 's/^rpc-listen-port='${aria2_port}'/rpc-listen-port='${aria2_RPC_port}'/g' ${aria2_conf}
+			if [[ $? -eq 0 ]];then
+				echo -e "${Info} 端口修改成功！新密码为：${Green_font_prefix}${aria2_RPC_port}${Font_color_suffix}"
+				Del_iptables
+				Add_iptables
+				Save_iptables
+				if [[ ${read_123} != "1" ]]; then
+					Restart_aria2
+				fi
+			else 
+				echo -e "${Error} 端口修改失败！旧密码为：${Green_font_prefix}${aria2_port}${Font_color_suffix}"
+			fi
+		fi
+	else
+		echo -e "${Error} 新端口与旧端口一致，取消..."
+	fi
+}
+Set_aria2_RPC_dir(){
+	read_123=$1
+	if [[ ${read_123} != "1" ]]; then
+		Read_config
+	fi
+	if [[ -z "${aria2_dir}" ]]; then
+		aria2_dir_1="空(没有检测到配置，可能手动删除或注释了)"
+	else
+		aria2_dir_1=${aria2_dir}
+	fi
+	echo -e "请输入要设置的 Aria2 文件下载位置(旧位置为：${Green_font_prefix}${aria2_dir_1}${Font_color_suffix})"
+	stty erase '^H' && read -p "(默认位置: /usr/local/caddy/www/aria2/Download):" aria2_RPC_dir
+	[[ -z "${aria2_RPC_dir}" ]] && aria2_RPC_dir="/usr/local/caddy/www/aria2/Download"
+	echo
+	if [[ -d "${aria2_RPC_dir}" ]]; then
+		if [[ "${aria2_dir}" != "${aria2_RPC_dir}" ]]; then
+			if [[ -z "${aria2_dir}" ]]; then
+				echo -e "\ndir=${aria2_RPC_dir}" >> ${aria2_conf}
+				if [[ $? -eq 0 ]];then
+					echo -e "${Info} 位置修改成功！新位置为：${Green_font_prefix}${aria2_RPC_dir}${Font_color_suffix}(因为找不到旧配置参数，所以自动加入配置文件底部)"
+					if [[ ${read_123} != "1" ]]; then
+						Restart_aria2
+					fi
+				else 
+					echo -e "${Error} 位置修改失败！旧位置为：${Green_font_prefix}${aria2_dir}${Font_color_suffix}"
+				fi
+			else
+				aria2_dir_2=$(echo "${aria2_dir}"|sed 's/\//\\\//g')
+				aria2_RPC_dir_2=$(echo "${aria2_RPC_dir}"|sed 's/\//\\\//g')
+				sed -i 's/^dir='${aria2_dir_2}'/dir='${aria2_RPC_dir_2}'/g' ${aria2_conf}
+				if [[ $? -eq 0 ]];then
+					echo -e "${Info} 位置修改成功！新位置为：${Green_font_prefix}${aria2_RPC_dir}${Font_color_suffix}"
+					if [[ ${read_123} != "1" ]]; then
+						Restart_aria2
+					fi
+				else 
+					echo -e "${Error} 位置修改失败！旧位置为：${Green_font_prefix}${aria2_dir}${Font_color_suffix}"
+				fi
+			fi
+		else
+			echo -e "${Error} 新位置与旧位置一致，取消..."
+		fi
+	else
+		echo -e "${Error} 新位置文件夹不存在，请检查！新位置为：${Green_font_prefix}${aria2_RPC_dir}${Font_color_suffix}"
+	fi
+}
+Set_aria2_RPC_passwd_port_dir(){
+	Read_config
+	Set_aria2_RPC_passwd "1"
+	Set_aria2_RPC_port "1"
+	Set_aria2_RPC_dir "1"
+	Restart_aria2
+}
+Set_aria2_vim_conf(){
+	Read_config
+	aria2_port_old=${aria2_port}
+	echo -e "${Tip} 手动修改配置文件须知（VIM 编辑器）：
+${Green_font_prefix}1.${Font_color_suffix} 配置文件中含有中文注释，如果你的服务器或SSH工具不支持中文现在，将会乱码(请本地编辑)。
+${Green_font_prefix}2.${Font_color_suffix} 打开配置文件后，按 ${Green_font_prefix}I键${Font_color_suffix} 进入编辑模式(左下角显示 ${Green_font_prefix}-- INSERT --${Font_color_suffix})，然后就可以编辑文件了，
+   编辑文件完成后，按 ${Green_font_prefix}Esc键${Font_color_suffix} 退出编辑模式，然后输入 ${Green_font_prefix}:wq${Font_color_suffix} (半角 小写)回车，即保存并退出。
+${Green_font_prefix}3.${Font_color_suffix} 如果你不想要保存，那么按 ${Green_font_prefix}Esc键${Font_color_suffix} 退出编辑模式，然后输入 ${Green_font_prefix}:q!${Font_color_suffix} (半角 小写)回车，即不保存退出。
+${Green_font_prefix}4.${Font_color_suffix} 如果你打算在本地编辑配置文件，那么配置文件位置： ${Green_font_prefix}/root/.aria2/aria2.conf${Font_color_suffix} (注意是隐藏目录) 。" && echo
+	stty erase '^H' && read -p "如果理解 VIM 使用方法，请按任意键继续，如要取消请使用 Ctrl+C 。" var
 	vim ${aria2_conf}
+	Read_config
+	if [[ ${aria2_port_old} != ${aria2_port} ]]; then
+		aria2_RPC_port=${aria2_port}
+		aria2_port=${aria2_port_old}
+		Del_iptables
+		Add_iptables
+		Save_iptables
+	fi
 	Restart_aria2
 }
 Read_config(){
 	[[ ! -e ${aria2_conf} ]] && echo -e "${Error} Aria2 配置文件不存在 !" && exit 1
 	conf_text=$(cat ${aria2_conf}|grep -v '#')
 	aria2_dir=$(echo -e "${conf_text}"|grep "dir="|awk -F "=" '{print $NF}')
-	aria2_rpc_port=$(echo -e "${conf_text}"|grep "rpc-listen-port="|awk -F "=" '{print $NF}')
-	aria2_rpc_secret=$(echo -e "${conf_text}"|grep "rpc-secret="|awk -F "=" '{print $NF}')
+	aria2_port=$(echo -e "${conf_text}"|grep "rpc-listen-port="|awk -F "=" '{print $NF}')
+	aria2_passwd=$(echo -e "${conf_text}"|grep "rpc-secret="|awk -F "=" '{print $NF}')
+}
+View_Aria2(){
+	check_installed_status
+	Read_config
+	ip=$(wget -qO- -t1 -T2 ipinfo.io/ip)
+	if [[ -z "${ip}" ]]; then
+		ip=$(wget -qO- -t1 -T2 api.ip.sb/ip)
+		if [[ -z "${ip}" ]]; then
+			ip=$(wget -qO- -t1 -T2 members.3322.org/dyndns/getip)
+			if [[ -z "${ip}" ]]; then
+				ip="VPS_IP(外网IP检测失败)"
+			fi
+		fi
+	fi
+	[[ -z "${aria2_dir}" ]] && aria2_dir="找不到配置参数"
+	[[ -z "${aria2_port}" ]] && aria2_port="找不到配置参数"
+	[[ -z "${aria2_passwd}" ]] && aria2_passwd="找不到配置参数(或无密码)"
+	clear
+	echo -e "\nAria2 简单配置信息：\n
+ 地址\t: ${Green_font_prefix}${ip}${Font_color_suffix}
+ 端口\t: ${Green_font_prefix}${aria2_port}${Font_color_suffix}
+ 密码\t: ${Green_font_prefix}${aria2_passwd}${Font_color_suffix}
+ 目录\t: ${Green_font_prefix}${aria2_dir}${Font_color_suffix}\n"
 }
 View_Log(){
 	[[ ! -e ${aria2_log} ]] && echo -e "${Error} Aria2 日志文件不存在 !" && exit 1
@@ -200,12 +403,12 @@ Uninstall_aria2(){
 	fi
 }
 Add_iptables(){
-	iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport ${aria2_rpc_port} -j ACCEPT
-	iptables -I INPUT -m state --state NEW -m udp -p udp --dport ${aria2_rpc_port} -j ACCEPT
+	iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport ${aria2_RPC_port} -j ACCEPT
+	iptables -I INPUT -m state --state NEW -m udp -p udp --dport ${aria2_RPC_port} -j ACCEPT
 }
 Del_iptables(){
-	iptables -D INPUT -m state --state NEW -m tcp -p tcp --dport ${aria2_rpc_port} -j ACCEPT
-	iptables -D INPUT -m state --state NEW -m udp -p udp --dport ${aria2_rpc_port} -j ACCEPT
+	iptables -D INPUT -m state --state NEW -m tcp -p tcp --dport ${aria2_port} -j ACCEPT
+	iptables -D INPUT -m state --state NEW -m udp -p udp --dport ${aria2_port} -j ACCEPT
 }
 Save_iptables(){
 	if [[ ${release} == "centos" ]]; then
@@ -255,7 +458,8 @@ echo && echo -e " Aria2 一键安装管理脚本 ${Red_font_prefix}[v${sh_ver}]$
  ${Green_font_prefix}5.${Font_color_suffix} 重启 Aria2
 ————————————
  ${Green_font_prefix}6.${Font_color_suffix} 修改 配置文件
- ${Green_font_prefix}7.${Font_color_suffix} 查看 日志信息
+ ${Green_font_prefix}7.${Font_color_suffix} 查看 配置信息
+ ${Green_font_prefix}8.${Font_color_suffix} 查看 日志信息
 ————————————" && echo
 if [[ -e ${aria2c} ]]; then
 	check_pid
@@ -292,6 +496,9 @@ case "$num" in
 	Set_aria2
 	;;
 	7)
+	View_Aria2
+	;;
+	8)
 	View_Log
 	;;
 	*)
