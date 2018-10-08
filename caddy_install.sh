2da3bc311a808cf86a4eaef0f7f1c60c0a8b2240
Toyo <toyo13140010096@gmail.com>
2018-10-08 13:18:51 +0800

    # v1.0.8 新增 支持 ARM(armv7l) 安装。

--- caddy_install.sh
+++ caddy_install.sh
@@ -57,7 +57,7 @@ Download_caddy(){
 	elif [[ ${bit} == "armv7l" ]]; then
 		wget --no-check-certificate -O "caddy_linux.tar.gz" "https://caddyserver.com/download/linux/arm7${extension_all}"
 	else
-		echo -e "${Error_font_prefix}[错误]${Font_suffix} 不支持 [${bit}] ! 请向本站反馈此处显示的名称，我看看是否能添加支出。" && exit 1
+		echo -e "${Error_font_prefix}[错误]${Font_suffix} 不支持 [${bit}] ! 请向本站反馈[]中的名称，我会看看是否可以添加支持。" && exit 1
 	fi
 	[[ ! -e "caddy_linux.tar.gz" ]] && echo -e "${Error_font_prefix}[错误]${Font_suffix} Caddy 下载失败 !" && exit 1
 	tar zxf "caddy_linux.tar.gz"
